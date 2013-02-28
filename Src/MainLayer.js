25bb0e95c754cc0ae410666f1eef28b60fe924f1
Paul Wood III <paul.wood.iii@gmail.com>
2013-02-28 11:53:23 +0800

    Added some notes in comments

--- Src/MainLayer.js
+++ Src/MainLayer.js
@@ -1,28 +1,30 @@
-// 1
+// 1 This creates a new class called MainLayer that extends from Cocos2D’s LayerColor class. Note that in Cocos2D Javascript bindings, all Cocos2D classes have the cc prefix.
 var MainLayer = cc.LayerColor.extend({
 	
 		_monsters:[],
- 
-    // 2
+		_projectiles:[],
+		
+    // 2 Creates a constructor for the class, that calls the superclass’s constructor.
     ctor:function() {
         this._super();
  
-        // 3
+        // 3 In Cocos2D Javascript bindings, whenever you derive from a Cocos2D class you have to call this method to associate it with the appropriate native class.
         cc.associateWithNative( this, cc.LayerColor );
     },
  
-    // 4
+    // 4 When a node is added for the first time to a scene, Cocos2D calls onEnter on it. So this is a good place to put initialization code for a layer.
     onEnter:function () {
         this._super();
  
-        // 5
+        // 5 This line creates a sprite and puts it in a variable named player. Note for the name of the sprite you pass in the constant s_player that you created earlier.
         var player = cc.Sprite.create(s_player);
  
-        // 6
+        // 6 Sets the position of the sprite to be the middle-left of the screen. winSize is a handy constant you will define later.
         player.setPosition(player.getContentSize().width / 2, winSize.height / 2);
  
-        // 7
+        // 7 Finally, adds the player sprite to the layer.
         this.addChild(player);
+				
 				
 				this.schedule(this.gameLogic, 3);
     },
@@ -60,6 +62,7 @@ var MainLayer = cc.LayerColor.extend({
 		    this._monsters.push(monster); // 6
  
 		},
+		// let’s make sure your addMonster function is called periodically.
 		gameLogic:function(dt) {
 		    this.addMonster();
 		}
